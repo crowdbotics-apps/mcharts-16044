9dd5e3a486cedc1e1844169acff731b18b1e3216
Crowdbotics <team@crowdbotics.com>
2020-04-20 06:04:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mcharts_16044/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mcharts_16044;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mcharts_16044";
+    }
+}
